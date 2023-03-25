bac566dc5a9c0cd05859c1f2095c21f85c2ac7e1
jimini1026 <128363259+jimini1026@users.noreply.github.com>
2023-03-25 19:01:26 +0900

    [Bronze III] Title: 최댓값, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.py
+++ 백준/Bronze/2562. 최댓값/최댓값.py
@@ -1,8 +1,4 @@
 list=[]
 for i in range(9):
     list.append(int(input()))
-max=0
-for g in range(9):
-    if max<list[g]:
-        max=list[g]
-print(max,list.index(max)+1,sep="\n")
+print(max(list),list.index(max(list))+1,sep="\n")
