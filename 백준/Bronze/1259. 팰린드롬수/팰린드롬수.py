64b5888ce4ec3e3619145bf417abbcf577098ab8
jimini1026 <128363259+jimini1026@users.noreply.github.com>
2023-04-27 23:25:53 +0900

    [Bronze I] Title: 팰린드롬수, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/1259. 팰린드롬수/팰린드롬수.py
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.py
@@ -1,17 +1,9 @@
 while True:
-    try:
-        number = input()
-        if number[0] != "0":
-            test = 0
-            for i in range(len(number)):
-                if number[i] == number[len(number)-i-1]:
-                    test += 1
-                    
-            if test == len(number):
-                print("yes")
-
-            else:   
-                print("no")
-
-    except:
+    number = input()
+    if number[0] == "0":
         break
+    else:
+        if number == number[::-1]:
+            print("yes")
+        else:
+            print("no")
