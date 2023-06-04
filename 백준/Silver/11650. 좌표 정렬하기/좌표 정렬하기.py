503855b9911218720b458890c69319099e85401f
jimini1026 <128363259+jimini1026@users.noreply.github.com>
2023-06-04 20:03:37 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 4168 ms, Memory: 59204 KB -BaekjoonHub

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.py
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.py
@@ -4,8 +4,7 @@ lst = [0] * N
 for indx in range(N):
     lst[indx] = list(map(int, input().split()))
 
-lst.sort(key = lambda y: y[1])
-lst.sort(key = lambda x: x[0])
+lst.sort(key = lambda x: (x[0], x[1]))
 
 for i in lst:
     print(*i)
