ca49a6c6df5a37400d5d930b5286137f4e8b7580
jimini1026 <128363259+jimini1026@users.noreply.github.com>
2023-07-08 22:58:55 +0900

    [Bronze II] Title: Hashing, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.py
+++ 백준/Bronze/15829. Hashing/Hashing.py
@@ -2,9 +2,4 @@ L = int(input())
 num = 0
 for i, alphabet in enumerate(input()):
     num += (ord(alphabet) - 96) * (31 ** i)
-
-M = 1234567891
-
-if num >= M:
-    num = num % M
-print(num)
+print(num % 1234567891)
