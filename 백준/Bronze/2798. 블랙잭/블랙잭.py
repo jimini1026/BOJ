f27a1dcb7a5d7b1249b3add25716d9c9f3dd9eed
jimini1026 <128363259+jimini1026@users.noreply.github.com>
2023-07-19 14:02:52 +0900

    [Bronze II] Title: 블랙잭, Time: 104 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.py
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -1,18 +1,11 @@
 N, M = map(int, input().split())
 card = list(map(int, input().split()))
 
-lst = []
+result = 0
 for indx_1, i in enumerate(card):
     for indx_2, j in enumerate(card[indx_1 + 1:], indx_1 + 1):
         for k in card[indx_2 + 1:]:
-            lst.append(i + j + k)
-
-lst.sort()
-
-for indx, i in enumerate(lst, 1):
-    if i > M:
-        break
-    else:
-        num = i
-
-print(num)
+            sum = i + j + k
+            if sum <= M:
+                result = max(result, sum)
+print(result)
