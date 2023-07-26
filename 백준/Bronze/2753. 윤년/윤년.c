3b5dbd2e2cf56d46880306cd7c64bfef4f2a508c
jimini1026 <128363259+jimini1026@users.noreply.github.com>
2023-07-26 17:22:23 +0900

    [Bronze V] Title: 윤년, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void){
+        int a;
+
+        scanf("%d", &a);
+
+        if (a%400 == 0){
+                printf("%d", 1);
+        }else{
+                if (a%4 == 0 & a%100 != 0){
+                        printf("%d", 1);
+                }else{
+                        printf("%d", 0);
+                }
+        }
+
+        return 0;
+}
